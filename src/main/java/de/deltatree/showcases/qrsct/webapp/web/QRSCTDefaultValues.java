fef17b49b439be850ca01803873f57a11b70f660
Alexander Widak <deltatree.widak@googlemail.com>
2018-10-31 13:01:18 +0100

    Update QRSCTDefaultValues.java

--- src/main/java/de/deltatree/showcases/qrsct/webapp/web/QRSCTDefaultValues.java
+++ src/main/java/de/deltatree/showcases/qrsct/webapp/web/QRSCTDefaultValues.java
@@ -2,7 +2,6 @@ package de.deltatree.showcases.qrsct.webapp.web;
 
 public class QRSCTDefaultValues {
 	public static final String HINT = "kein Hinweis"; //$NON-NLS-1$
-	public static final String REFERENCE = "egon 123 test : dankeschoen"; //$NON-NLS-1$
 	public static final String PURPOSE = "OTHR"; //$NON-NLS-1$
 	public static final String AMOUNT = "999999999.11"; //$NON-NLS-1$
 	public static final String AMOUNT_CURRENCY_CODE = "EUR"; //$NON-NLS-1$
@@ -12,4 +11,5 @@ public class QRSCTDefaultValues {
 	public static final String CHARACTER_SET = "UTF_8"; //$NON-NLS-1$
 	public static final String VERSION = "V1"; //$NON-NLS-1$
 	public static final String SERVICE_TAG = "BCD"; //$NON-NLS-1$
+	public static final String TEXT = "egon 123 test : dankeschoen";
 }
